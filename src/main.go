8e3fb5b43d72e5ab8a36a1a47b1168705c7eebfc
maksimpervickij <maksim_perv@mail.ru>
2024-12-03 20:47:33 +0300

    Решение задачи 2: преобразование скорости в км/ч и миль/ч

--- src/main.go
+++ src/main.go
@@ -1,14 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
+
+type AmericanVelocity float64
+
+type EuropeanVelocity float64
 
 func main() {
-	a := "104"
-	b := 35
-	a1, _ := strconv.Atoi(a)
-	b1 := strconv.Itoa(b)
-	fmt.Println(a1, b1)
+	// Константы для преобразования
+	const mpsToKmph = 3.6    // метры в секунду в километры в час
+	const mpsToMph = 2.23694 // метры в секунду в мили в час
+
+	// Преобразование скорости 120.4 м/сек в км/ч
+	var speedInMps1 float64 = 120.4
+	var speedInKmph EuropeanVelocity = EuropeanVelocity(speedInMps1 * mpsToKmph)
+
+	// Преобразование скорости 130 м/с в миль/ч
+	var speedInMps2 float64 = 130
+	var speedInMph AmericanVelocity = AmericanVelocity(speedInMps2 * mpsToMph)
+
+	// Вывод результатов
+	fmt.Printf("Скорость %.2f м/с равна %.2f км/ч\n", speedInMps1, speedInKmph)
+	fmt.Printf("Скорость %.2f м/с равна %.2f миль/ч\n", speedInMps2, speedInMph)
 }
